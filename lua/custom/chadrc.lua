516df23708b6228ccc69acc7b3ddf6bc9217ed9b
LeonardsonCC <leonardson.carvalho@gmail.com>
2023-07-06 14:53:01 -0300

    lazy update

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "everforest",
-  theme_toggle = { "everforest", "one_light" },
+  theme = "dark_horizon",
+  theme_toggle = { "dark_horizon", "one_light" },
   transparency = true,
 
   statusline = {
