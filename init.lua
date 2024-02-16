81cf15d5060a206f722195a1d288129d3d8abc7e
LeonardsonCC <leonardson.carvalho@gmail.com>
2024-02-16 13:43:49 -0300

    colorscheme

--- init.lua
+++ init.lua
@@ -68,7 +68,7 @@ vim.opt.rtp:prepend(lazypath)
 --    as they will be available in your neovim runtime.
 require('lazy').setup({
   install = {
-    colorscheme = { 'cyberdream', 'habamax' },
+    colorscheme = { 'rose-pine', 'habamax' },
   },
   change_detection = {
     -- automatically check for config file changes and reload the ui
@@ -125,24 +125,67 @@ require('lazy').setup({
 
       -- Adds a number of user-friendly snippets
       'rafamadriz/friendly-snippets',
+
+      -- kind icons
+      'onsails/lspkind.nvim',
     },
   },
 
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim', opts = {} },
   {
-    'scottmckendry/cyberdream.nvim',
+    'rose-pine/neovim',
     lazy = false,
     priority = 1000,
     config = function()
-      require('cyberdream').setup({
-        -- Recommended - see "Configuring" below for more config options
-        transparent = true,
-        italic_comments = true,
-        hide_fillchars = true,
-        borderless_telescope = false,
+      require('rose-pine').setup({
+        styles = {
+          transparency = true,
+        },
+        highlight_groups = {
+          PmenuSel = { bg = '#282C34', fg = 'NONE' },
+          Pmenu = { fg = '#C5CDD9', bg = '#22252A' },
+
+          CmpItemAbbrDeprecated = { fg = '#7E8294', bg = 'NONE', strikethrough = true },
+          CmpItemAbbrMatch = { fg = '#82AAFF', bg = 'NONE', bold = true },
+          CmpItemAbbrMatchFuzzy = { fg = '#82AAFF', bg = 'NONE', bold = true },
+          CmpItemMenu = { fg = '#C792EA', bg = 'NONE', italic = true },
+
+          CmpItemKindField = { fg = '#EED8DA', bg = '#B5585F' },
+          CmpItemKindProperty = { fg = '#EED8DA', bg = '#B5585F' },
+          CmpItemKindEvent = { fg = '#EED8DA', bg = '#B5585F' },
+
+          CmpItemKindText = { fg = '#C3E88D', bg = '#9FBD73' },
+          CmpItemKindEnum = { fg = '#C3E88D', bg = '#9FBD73' },
+          CmpItemKindKeyword = { fg = '#C3E88D', bg = '#9FBD73' },
+
+          CmpItemKindConstant = { fg = '#FFE082', bg = '#D4BB6C' },
+          CmpItemKindConstructor = { fg = '#FFE082', bg = '#D4BB6C' },
+          CmpItemKindReference = { fg = '#FFE082', bg = '#D4BB6C' },
+
+          CmpItemKindFunction = { fg = '#EADFF0', bg = '#A377BF' },
+          CmpItemKindStruct = { fg = '#EADFF0', bg = '#A377BF' },
+          CmpItemKindClass = { fg = '#EADFF0', bg = '#A377BF' },
+          CmpItemKindModule = { fg = '#EADFF0', bg = '#A377BF' },
+          CmpItemKindOperator = { fg = '#EADFF0', bg = '#A377BF' },
+
+          CmpItemKindVariable = { fg = '#C5CDD9', bg = '#7E8294' },
+          CmpItemKindFile = { fg = '#C5CDD9', bg = '#7E8294' },
+
+          CmpItemKindUnit = { fg = '#F5EBD9', bg = '#D4A959' },
+          CmpItemKindSnippet = { fg = '#F5EBD9', bg = '#D4A959' },
+          CmpItemKindFolder = { fg = '#F5EBD9', bg = '#D4A959' },
+
+          CmpItemKindMethod = { fg = '#DDE5F5', bg = '#6C8ED4' },
+          CmpItemKindValue = { fg = '#DDE5F5', bg = '#6C8ED4' },
+          CmpItemKindEnumMember = { fg = '#DDE5F5', bg = '#6C8ED4' },
+
+          CmpItemKindInterface = { fg = '#D8EEEB', bg = '#58B5A8' },
+          CmpItemKindColor = { fg = '#D8EEEB', bg = '#58B5A8' },
+          CmpItemKindTypeParameter = { fg = '#D8EEEB', bg = '#58B5A8' },
+        },
       })
-      vim.cmd('colorscheme cyberdream') -- set the colorscheme
+      vim.cmd('colorscheme rose-pine') -- set the colorscheme
     end,
   },
 
@@ -423,11 +466,28 @@ cmp.setup({
     end,
   },
   window = {
-    completion = cmp.config.window.bordered(),
+    -- completion = cmp.config.window.bordered(),
+    completion = {
+      winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,Search:None',
+      col_offset = -3,
+      side_padding = 0,
+    },
     documentation = cmp.config.window.bordered(),
   },
   completion = {
     completeopt = 'menu,menuone,noinsert',
+  },
+  formatting = {
+    expandable_indicator = true,
+    fields = { 'kind', 'abbr', 'menu' },
+    format = function(entry, vim_item)
+      local kind = require('lspkind').cmp_format({ mode = 'symbol_text', maxwidth = 50 })(entry, vim_item)
+      local strings = vim.split(kind.kind, '%s', { trimempty = true })
+      kind.kind = ' ' .. (strings[1] or '') .. ' '
+      kind.menu = '    (' .. (strings[2] or '') .. ')'
+
+      return kind
+    end,
   },
   mapping = cmp.mapping.preset.insert({
     ['<C-n>'] = cmp.mapping.select_next_item(),
--- lua/custom/plugins/ts-context.lua
+++ lua/custom/plugins/ts-context.lua
@@ -1,7 +1,10 @@
 return {
   'nvim-treesitter/nvim-treesitter-context',
-  opts = {},
   config = function()
+    require('treesitter-context').setup({
+      enable = true,
+      max_lines = 5,
+    })
     vim.cmd([[hi TreesitterContext guibg=#222222]])
   end,
 }
