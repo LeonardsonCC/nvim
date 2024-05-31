18755df208b6741e84b755e3a546cb45a479ef96
LeonardsonCC <leonardson.carvalho@gmail.com>
2024-05-31 16:25:00 -0300

    github

---
+++ lua/plugins/github.lua
@@ -0,0 +1,13 @@
+return {
+  "pwntester/octo.nvim",
+  requires = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    suppress_missing_scope = {
+      projects_v2 = true,
+    },
+  },
+}
