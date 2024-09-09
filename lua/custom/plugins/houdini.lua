60748f3cbdc11e47080fb1e7d9b4632021d3586e
LeonardsonCC <leonardson.carvalho@gmail.com>
2024-09-09 09:52:09 -0300

    temp

--- lua/custom/plugins/houdini.lua
+++ lua/custom/plugins/houdini.lua
@@ -1,6 +1,6 @@
 return {
-	"TheBlob42/houdini.nvim",
-	config = function()
-		require("houdini").setup()
-	end,
+	-- "TheBlob42/houdini.nvim",
+	-- config = function()
+	-- 	require("houdini").setup()
+	-- end,
 }
