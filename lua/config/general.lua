bed71cab2a9acaec89aaee432e15683daee0ce63
LeonardsonCC <leonardson.carvalho@gmail.com>
2025-07-08 10:33:42 -0300

    fix: update multi-cursor key mapping for VSCode integration
    - Removed the normal mode mapping for the multi-cursor key binding to streamline functionality.
    - Adjusted key mapping to only include visual, insert, and select modes for improved usability.

--- lua/config/general.lua
+++ lua/config/general.lua
@@ -22,7 +22,7 @@ vim.keymap.set("n", "<leader>sg", function()
 end)
 
 -- multi cursor
-vim.keymap.set({ "n", "x", "i", "v" }, "<C-i>", function()
+vim.keymap.set({ "x", "i", "v" }, "<C-i>", function()
   vscode.with_insert(function()
     vscode.action("editor.action.addSelectionToNextFindMatch")
   end)
